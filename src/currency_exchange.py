8a27493acd9b16b071d6363f71d95750349600a9
AranParrish <153287399+AranParrish@users.noreply.github.com>
2024-06-07 19:14:22 +0100

    add tests and solutions for extract function

--- src/currency_exchange.py
+++ src/currency_exchange.py
@@ -2,25 +2,44 @@ import requests
 from requests import HTTPError
 import json
 
+API_MAIN_SOURCE_PREFIX = (
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/"
+)
+API_FALLBACK_SOURCE_PREFIX = "https://latest.currency-api.pages.dev/v1/currencies/"
+
+
 def extract_currency_rates(base_currency: str) -> dict:
-    
-    base_currency = base_currency.lower()
 
-    api_main_source = f'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{base_currency}.json'
-    api_fallback_source = f'https://latest.currency-api.pages.dev/v1/currencies/{base_currency}.json'
+    base_currency_insensitive = base_currency.lower()
 
-    api_response = requests.get(api_main_source)
-
+    api_response = requests.get(
+        f"{API_MAIN_SOURCE_PREFIX}{base_currency_insensitive}.json"
+    )
     if api_response.status_code == 200:
 
         currency_rates = json.loads(api_response.text)
         return currency_rates
-    
+
+    elif api_response.status_code == 500:
+
+        fallback_response = requests.get(
+            f"{API_FALLBACK_SOURCE_PREFIX}{base_currency_insensitive}.json"
+        )
+        if fallback_response.status_code == 200:
+
+            currency_rates = json.loads(fallback_response.text)
+            return currency_rates
+
+        elif fallback_response.status_code == 500:
+            raise HTTPError("Servers busy, try again later")
+
     elif api_response.status_code == 404:
-        raise HTTPError('Invalid input: Enter base currency in shorthand format')
+        raise HTTPError("Invalid input: Enter base currency in shorthand format")
+
 
 def transform_currency_rates(extracted_data: dict) -> dict:
     pass
+
 
 def load_currency_rates(transformed_data: dict):
     pass
--- test/test_currency_exchange.py
+++ test/test_currency_exchange.py
@@ -1,28 +1,42 @@
 import pytest
 from requests import HTTPError
-from src.currency_exchange import extract_currency_rates, transform_currency_rates, \
-    load_currency_rates
+from unittest.mock import Mock, patch
+from src.currency_exchange import (
+    extract_currency_rates,
+    transform_currency_rates,
+    load_currency_rates,
+)
 
-@pytest.mark.describe('Extract currency rates tests')
+
+@pytest.mark.describe("Extract currency rates tests")
 class TestExtract:
 
-    @pytest.mark.it('Returns gbp currency rates dict')
+    @pytest.mark.it("Returns gbp currency rates dict")
     def test_extract_given_currency_rates(self):
-        test_currency = 'gbp'
+        test_currency = "gbp"
         output = extract_currency_rates(test_currency)
-        assert isinstance(output['gbp'], dict)
-        assert isinstance(output['gbp']['eur'], float)
+        assert isinstance(output["gbp"], dict)
+        assert isinstance(output["gbp"]["eur"], float)
 
-    @pytest.mark.it('Raises error for invalid currency')
+    @pytest.mark.it("Raises error for invalid currency")
     def test_extract_invalid_currency(self):
-        invalid_currency = 'gbbp'
+        invalid_currency = "gbbp"
         with pytest.raises(HTTPError) as e:
             extract_currency_rates(invalid_currency)
         assert "base currency in shorthand format" in str(e.value)
 
-    @pytest.mark.it('Function is case insensitive')
+    @pytest.mark.it("Function is case insensitive")
     def test_extract_case_insensitive(self):
-        test_currency = 'GBP'
+        test_currency = "GBP"
         output = extract_currency_rates(test_currency)
-        assert isinstance(output['gbp'], dict)
-        assert isinstance(output['gbp']['eur'], float)
+        assert isinstance(output["gbp"], dict)
+        assert isinstance(output["gbp"]["eur"], float)
+
+    @pytest.mark.it("Raises error if both servers busy")
+    def test_extract_fallback_if_main_busy(self):
+        test_currency = "GBP"
+        with patch("src.currency_exchange.requests.get") as mock_request:
+            mock_request.return_value.status_code = 500
+            with pytest.raises(HTTPError) as err:
+                extract_currency_rates(test_currency)
+            assert "Servers busy" in str(err.value)
