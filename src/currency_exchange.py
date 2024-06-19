98458afa627567ebd9897715c71dae66962f1bda
AranParrish <153287399+AranParrish@users.noreply.github.com>
2024-06-19 11:44:55 +0100

    add tests and solution for transform function

--- src/currency_exchange.py
+++ src/currency_exchange.py
@@ -2,19 +2,27 @@ import requests
 from requests import HTTPError
 import json
 
-API_MAIN_SOURCE_PREFIX = (
-    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/"
-)
-API_FALLBACK_SOURCE_PREFIX = "https://latest.currency-api.pages.dev/v1/currencies/"
+API_MAIN_SOURCE = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/gbp.json"
+API_FALLBACK_SOURCE = "https://latest.currency-api.pages.dev/v1/currencies/gbp.json"
 
 
-def extract_currency_rates(base_currency: str) -> dict:
+def extract_currency_rates() -> dict:
+    """Extracts today's currency exchange rates for great british pounds (GBP).
 
-    base_currency_insensitive = base_currency.lower()
+    Extracts today's GBP currency rates using the provided API source,
+    using the fallback API source if that is busy.
 
-    api_response = requests.get(
-        f"{API_MAIN_SOURCE_PREFIX}{base_currency_insensitive}.json"
-    )
+    Args:
+        None
+
+    Returns:
+        A dictionary of currency rates against the base currency
+
+    Raises:
+        HTTPError if both servers busy
+    """
+
+    api_response = requests.get(API_MAIN_SOURCE)
     if api_response.status_code == 200:
 
         currency_rates = json.loads(api_response.text)
@@ -22,9 +30,7 @@ def extract_currency_rates(base_currency: str) -> dict:
 
     elif api_response.status_code == 500:
 
-        fallback_response = requests.get(
-            f"{API_FALLBACK_SOURCE_PREFIX}{base_currency_insensitive}.json"
-        )
+        fallback_response = requests.get(API_FALLBACK_SOURCE)
         if fallback_response.status_code == 200:
 
             currency_rates = json.loads(fallback_response.text)
@@ -33,12 +39,44 @@ def extract_currency_rates(base_currency: str) -> dict:
         elif fallback_response.status_code == 500:
             raise HTTPError("Servers busy, try again later")
 
-    elif api_response.status_code == 404:
-        raise HTTPError("Invalid input: Enter base currency in shorthand format")
 
+def transform_currency_rates(
+    extracted_data: dict, currencies_list: list = ["eur", "usd"]
+) -> dict:
+    """Transform currency exchange rate data to give rate and reverse rate.
 
-def transform_currency_rates(extracted_data: dict) -> dict:
-    pass
+    Transforms the inputted currency exchange rate data to provide the user with rate and
+    reverse rate values against the base currency of GBP.  By default, provides just EUR
+    and USD rates, but can optionally take a list of currencies to return.
+
+    Args:
+        extracted_data: GBP exchange rate source data
+        (optional) currencies_list: List of currency rates to be returned, defaults to EUR and USD.
+
+    Returns:
+        A dictionary providing the rate and reverse rate against GBP for the specified currencies.
+
+    Raises:
+        TypeError if either input is not of the expected type
+        KeyError if any currency in currencies_list not found in the extracted_data
+    """
+    currencies = {}
+    if isinstance(extracted_data, dict) and isinstance(currencies_list, list):
+
+        for currency in currencies_list:
+            try:
+                currencies[currency] = {
+                    "rate": extracted_data["gbp"][currency],
+                    "reverse_rate": 1 / extracted_data["gbp"][currency],
+                }
+            except KeyError:
+                err_text = f"{currency} is not a valid currency code"
+                raise KeyError(err_text)
+
+        return currencies
+
+    else:
+        raise TypeError("Invalid input format")
 
 
 def load_currency_rates(transformed_data: dict):
--- test/test_currency_exchange.py
+++ test/test_currency_exchange.py
@@ -1,42 +1,115 @@
 import pytest
 from requests import HTTPError
 from unittest.mock import Mock, patch
+import boto3
+from moto import mock_aws
+from os import environ
 from src.currency_exchange import (
     extract_currency_rates,
     transform_currency_rates,
     load_currency_rates,
 )
 
+@pytest.fixture(scope="class")
+def aws_credentials():
+    environ["AWS_ACCESS_KEY_ID"] = "test"
+    environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    environ["AWS_SECURITY_TOKEN"] = "test"
+    environ["AWS_SESSION_TOKEN"] = "test"
+    environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
+@pytest.fixture(scope="function")
+def s3_client(aws_credentials):
+    with mock_aws():
+        yield boto3.client("s3")
+
+@pytest.fixture(scope="function")
+def test_bucket(s3_client):
+    s3_client.create_bucket(
+        Bucket="test_bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+    
 @pytest.mark.describe("Extract currency rates tests")
 class TestExtract:
 
     @pytest.mark.it("Returns gbp currency rates dict")
     def test_extract_given_currency_rates(self):
-        test_currency = "gbp"
-        output = extract_currency_rates(test_currency)
-        assert isinstance(output["gbp"], dict)
-        assert isinstance(output["gbp"]["eur"], float)
-
-    @pytest.mark.it("Raises error for invalid currency")
-    def test_extract_invalid_currency(self):
-        invalid_currency = "gbbp"
-        with pytest.raises(HTTPError) as e:
-            extract_currency_rates(invalid_currency)
-        assert "base currency in shorthand format" in str(e.value)
-
-    @pytest.mark.it("Function is case insensitive")
-    def test_extract_case_insensitive(self):
-        test_currency = "GBP"
-        output = extract_currency_rates(test_currency)
+        output = extract_currency_rates()
         assert isinstance(output["gbp"], dict)
         assert isinstance(output["gbp"]["eur"], float)
 
     @pytest.mark.it("Raises error if both servers busy")
     def test_extract_fallback_if_main_busy(self):
-        test_currency = "GBP"
         with patch("src.currency_exchange.requests.get") as mock_request:
             mock_request.return_value.status_code = 500
             with pytest.raises(HTTPError) as err:
-                extract_currency_rates(test_currency)
+                extract_currency_rates()
             assert "Servers busy" in str(err.value)
+
+
+@pytest.mark.describe("Transform currency rates tests")
+class TestTransform:
+
+    @pytest.mark.it("Output ref is not input ref")
+    def test_transform_output_not_input(self):
+        test_data = extract_currency_rates()
+        output = transform_currency_rates(test_data)
+        assert output is not test_data
+
+    @pytest.mark.it("Inputs not mutated")
+    def test_transform_inputs_not_mutated(self):
+        test_data = extract_currency_rates()
+        copy_test_data = extract_currency_rates()
+        user_currencies = ["btc", "eth"]
+        copy_user_currencies = ["btc", "eth"]
+        transform_currency_rates(test_data, user_currencies)
+        assert test_data == copy_test_data
+        assert user_currencies == copy_user_currencies
+
+    @pytest.mark.it("Returns EUR and USD dictionaries by default")
+    def test_transform_output_format(self):
+        test_data = extract_currency_rates()
+        output = transform_currency_rates(test_data)
+        assert "eur" in output.keys()
+        assert "usd" in output.keys()
+        for currency in output.keys():
+            assert isinstance(output[currency]["rate"], float)
+            assert isinstance(output[currency]["reverse_rate"], float)
+
+    @pytest.mark.it("Returns user defined currency rates")
+    def test_transform_user_currencies(self):
+        test_data = extract_currency_rates()
+        user_currencies = ["btc", "eth"]
+        output = transform_currency_rates(test_data, user_currencies)
+        assert all([currency in output.keys() for currency in user_currencies])
+
+    @pytest.mark.it("Raises TypeError for invalid input data")
+    def test_transform_invalid_input_data(self):
+        invalid_test_data = ["gbp", ["eur", "usd"]]
+        with pytest.raises(TypeError) as err:
+            transform_currency_rates(invalid_test_data)
+        assert "Invalid input format" in str(err.value)
+
+    @pytest.mark.it("Raises TypeError for invalid currencies list")
+    def test_transform_invalid_currencies_list(self):
+        test_data = extract_currency_rates()
+        invalid_currencies_list = ("btc", "eth")
+        with pytest.raises(TypeError) as err:
+            transform_currency_rates(test_data, invalid_currencies_list)
+        assert "Invalid input format" in str(err.value)
+
+    @pytest.mark.it("Raises KeyError for invalid currency")
+    def test_transform_invalid_currency(self):
+        test_data = extract_currency_rates()
+        invalid_currency = ["usd", "eurr"]
+        with pytest.raises(KeyError) as err:
+            transform_currency_rates(test_data, invalid_currency)
+        assert f"{invalid_currency[1]} is not a valid currency code" in str(err.value)
+
+@pytest.mark.describe("Load currency rates tests")
+class TestLoad:
+
+    @pytest.mark.it("Load transformed data into S3 bucket")
+    def test_load_transformed_data_into_s3(self):
+        pass
